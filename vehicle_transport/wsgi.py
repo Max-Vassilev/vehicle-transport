55ee50df47b2f9e6e901bf9ea03156e5c445f555
Max Vassilev <maximvassilev01@gmail.com>
2025-01-12 14:43:07 +0200

    Test

--- vehicle_transport/wsgi.py
+++ vehicle_transport/wsgi.py
@@ -4,6 +4,6 @@ from django.core.wsgi import get_wsgi_application
 
 settings_module = 'vehicle_transport.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'vehicle_transport.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicle_transport.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
